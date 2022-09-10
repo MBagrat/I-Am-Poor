f3029e3385a3aa205ebc1597755bc3ed8f3538eb
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2022-09-11 00:33:41 +0400

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Bagrat Mukaelyan on 11.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
